2156325a14ef96f24242303abd440600ca658b40
mysterymethod <chandra.pranay@yahoo.in>
2020-11-11 15:49:44 +0530

    implement function of inc dec and reset

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
  let count = 0;
 
- const value = document.getElementById('value');
+ const value = document.querySelector('.value');
  const btns = document.querySelectorAll('.btn');
 
  btns.forEach(function (btn){
      btn.addEventListener('click', function (event) {
-         console.log(event.currentTarget);
+         const classes = event.currentTarget.classList;
+         
+         if (classes.contains('decrease')) {
+            count--;
+         } 
+         else if (classes.contains('increase')) {
+            count ++;
+         }
+         else if (classes.contains('reset')) {
+             count = 0;
+         }
+
+         if (count > 0) {
+            value.style.color = 'green';
+         }
+         else if (count < 0) {
+            value.style.color = 'red';
+         }
+
+         else if (count == 0) {
+            value.style.color = 'black';
+         }
+
+         value.textContent = count;
      })
  });
 
